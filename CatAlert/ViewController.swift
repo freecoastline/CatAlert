8ed37b7c985bae6340b52290170199dfd5701458
wuyawei <wuyawei.ken@bytedance.com>
2025-09-06 11:26:28 +0800

    Initial Commit

---
+++ CatAlert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatAlert
+//
+//  Created by ByteDance on 2025/9/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CatAlertTests/CatAlertTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatAlertTests.swift
+//  CatAlertTests
+//
+//  Created by ByteDance on 2025/9/6.
+//
+
+import Testing
+@testable import CatAlert
+
+struct CatAlertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
