459a232d00b1f0e26c4d2c06e415181adfabc6cb
wuyawei <wuyawei.ken@bytedance.com>
2025-09-06 11:31:00 +0800

    init cat alert

--- CatAlert/ViewController.swift
+++ CatAlert/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let label = UILabel()
+        label.text = "Hello world"
+        label.textAlignment = .center
+        label.sizeToFit()
+        view.addSubview(label)
+        label.sn
     }
 
 
