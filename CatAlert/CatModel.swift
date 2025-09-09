12a671fdf4cbc3a08d4d1e24d908d5598fc24bc9
ken <jitamm20081@gmail.com>
2025-09-09 09:02:42 +0800

    feat:add cat Model

---
+++ CatAlert/CatModel.swift
@@ -0,0 +1,16 @@
+//
+//  CatModel.swift
+//  CatAlert
+//
+//  Created by ken on 2025/9/9.
+//
+
+import Foundation
+
+struct CatModel {
+    var name:String
+    var gendre:String
+    var kind:String
+    var description:String
+    var bornWay:String
+}
