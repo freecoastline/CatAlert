a366dfefddb8a77bb6fce89d51eb8a87cc0648eb
ken <jitamm20081@gmail.com>
2025-09-09 08:53:38 +0800

    feat:add pink background

--- CatAlert/CatProfileViewController.swift
+++ CatAlert/CatProfileViewController.swift
@@ -11,7 +11,20 @@ import UIKit
 class CatProfileViewController:UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
+        let backgroundImageView = UIImageView(frame: self.view.bounds)
+        let filePath = try Bundle.main.path(forResource: "PinkbackGround", ofType: "webp")
+        guard let filePath else {
+            backgroundImageView.backgroundColor = .systemPink
+            view.insertSubview(backgroundImageView, at: 0)
+            return
+        }
+        if let image = UIImage(contentsOfFile: filePath) {
+            backgroundImageView.image = image
+        } else {
+            backgroundImageView.backgroundColor = .systemPink
+        }
+        backgroundImageView.contentMode = .scaleAspectFill
+        view.insertSubview(backgroundImageView, at: 0)
         print("\(view.frame)")
     }
     
