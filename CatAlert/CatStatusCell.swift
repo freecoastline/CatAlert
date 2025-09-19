f53d40dd5f7d2e35bae3143248e2e6d80d8d4933
ken <jitamm20081@gmail.com>
2025-09-19 08:59:52 +0800

    feat:add daily care button

--- CatAlert/CatStatusCell.swift
+++ CatAlert/CatStatusCell.swift
@@ -9,9 +9,45 @@ import Foundation
 import UIKit
 
 class CatStatusCell:UITableViewCell {
+    let checkListButton = UIButton()
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.backgroundColor = .blue
+        contentView.addSubview(checkListButton)
+        checkListButton.setTitle("Daily Care", for: .normal)
+        checkListButton.layer.cornerRadius = 15
+        checkListButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(10)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(60)
+        }
+        checkListButton.contentHorizontalAlignment = .left
+        checkListButton.backgroundColor = .purple
+        checkListButton.addTarget(self, action: #selector(touchDown), for: .touchDown)
+        checkListButton.addTarget(self, action: #selector(touchUpInside), for: .touchUpInside)
+        checkListButton.addTarget(self, action: #selector(touchUpOutside), for: .touchUpOutside)
+    }
+    
+    
+    @objc
+    func touchDown() {
+        checkListButton.backgroundColor = .blue
+    }
+    
+    
+    @objc
+    func touchUpInside() {
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            guard let self else {
+                return
+            }
+            checkListButton.backgroundColor = .purple
+        }
+    }
+    
+    @objc
+    func touchUpOutside() {
+        checkListButton.backgroundColor = .purple
     }
     
     required init?(coder: NSCoder) {
