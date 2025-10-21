66369a3ea302ad50680ac4131723f8ae4306540b
ken <jitamm20081@gmail.com>
2025-10-21 09:20:09 +0800

    更新CatProfileViewControllerNew.swift

--- CatAlert/CatProfileViewControllerNew.swift
+++ CatAlert/CatProfileViewControllerNew.swift
@@ -9,5 +9,22 @@ import Foundation
 import UIKit
 
 class CatProfileViewControllerNew: UIViewController {
+    private lazy var scrollView = {
+        let view = UIScrollView()
+        return view
+    }()
     
+    private lazy var contentView = {
+        let view = UIView()
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        
+    }
 }
